fc662766ec7883b4ab97b3b72c0a101982b50085
shashidhar001 <shashidharkr2001@gmail.com>
2025-07-03 09:33:20 +0530

    now it's second commit // --- a/file:///c%3A/Users/Aruna/Desktop/Reactclass/Firstproject/src/App.jsx // +++ b/file:///c%3A/Users/Ar  na/Desktop/Reactclass/Firstproject/src/App.jsx // @@ -1,7 +1,7 @@ //  import React from "react";

--- Firstproject/src/App.jsx
+++ Firstproject/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import './App.css'
 function App(){
-  let style1={height:"10px",width:"20px",backgroundColor:"white"}
+  
   return(
     <>
-    <h1 style={{height:"5px",width:"1px",border:"",backgroundColor:"aqua"}}>Anything</h1>
-    <p style={{height:"5px",width:"1px",border:"",backgroundColor:"aqua"}}>Everything </p>
-    <p style={style1}>Lorem ipsum dolor sit amet consectetur </p>
+      <h1 className="heading">Hello World</h1>
+      <p className="paragraph">This is my first React project.</p>
     
     </>
   );
